bd332e0185c9a84f71b502b47d8da3d38af7d857
simonapo <37187003+simonapo@users.noreply.github.com>
2021-03-29 09:44:29 +0200

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,29 @@
 <body>
 
 <?php echo "Hello World v2!"; ?>
-<?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?>
+<?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?><br /><br />
+
+<?php $links = [];
+  foreach($_ENV as $key => $value) {
+    if(preg_match("/^(.*)_PORT_([0-9]*)_(TCP|UDP)$/", $key, $matches)) {
+      $links[] = [
+        "name"  => $matches[1],
+        "port"  => $matches[2],
+        "proto" => $matches[3],
+        "value" => $value
+      ];
+    }
+  }
+
+  if($links) {
+    foreach($links as $link) {
+      echo $link["name"]; ?>  listening on port <?php echo $link["port"]."/".$link["proto"]; ?> available at <?php echo $link["value"]; ?><br /><?php
+    }
+  }
+
+}
+?>
+
 
 </body>
 </html>
