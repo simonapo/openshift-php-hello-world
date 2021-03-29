68afa64e9462bf6b06aa1769db4dfbfd8287a644
simonapo <37187003+simonapo@users.noreply.github.com>
2021-03-29 09:52:45 +0200

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <body>
 
 <?php echo "Hello World v2!"; ?>
-<?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?>
+<?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"] ?>
 
 </body>
 </html>
